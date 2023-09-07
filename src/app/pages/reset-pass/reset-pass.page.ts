3867332da3e25b6a83481b1d01cc993165dc66a5
Maxcg98 <max.cruzg@duocuc.cl>
2023-09-07 19:44:41 -0300

    porfin

--- src/app/pages/reset-pass/reset-pass.page.ts
+++ src/app/pages/reset-pass/reset-pass.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-pass',
@@ -8,19 +9,52 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class ResetPassPage implements OnInit {
+  nombre: string = '';
+  contrasena: string = '';
+
+
+  usuario = {
+    contrasena_usuario: ''  ,
+  };
+
 
   isAlertOpen = false;
-  public alertButtons = ['OK']; 
+  public alertButtons = ['OK'];
   mensaje: string = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+
+  }
+
+  actualizar(){
+
+    if (this.nombre == '' && this.contrasena == '') {
+      this.mensaje = 'Debe ingresar valores';
+      this.isAlertOpen = true; 
+    } else {
+      
+      // Almacena los datos de la contraseña del usuario en el localStorage
+      localStorage.setItem('usuario', JSON.stringify(this.usuario));
+      this.usuario.contrasena_usuario = this.contrasena;
+      localStorage.setItem('usuario.contrasena_usuario', JSON.stringify(this.usuario.contrasena_usuario));
+
+      // Obtiene los datos de la contraseña del usuario del localStorage para que no lleguen vacios 
+      localStorage.getItem('usuario.contrasena_usuario');
+      
+      
+      this.router.navigate(['inicio']).then(() => {
+        location.reload(); 
+      }); 
+    }
 
   }
 
   setOpen(isOpen: boolean) {
     this.isAlertOpen = isOpen;
   }
+
+
 
 }
